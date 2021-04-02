43b000f9e58b04689e12ba861d05983914c78edc
Nate Nolting <41244351+natenolt@users.noreply.github.com>
2021-04-02 15:24:24 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace PayCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.rateTxtBox = new System.Windows.Forms.TextBox();
+            this.hourTxtBox = new System.Windows.Forms.TextBox();
+            this.hourLbl = new System.Windows.Forms.Label();
+            this.rateLbl = new System.Windows.Forms.Label();
+            this.computePayBtn = new System.Windows.Forms.Button();
+            this.empIDTxtBox = new System.Windows.Forms.TextBox();
+            this.nameTxtBox = new System.Windows.Forms.TextBox();
+            this.nameLbl = new System.Windows.Forms.Label();
+            this.empLbl = new System.Windows.Forms.Label();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(292, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.clearToolStripMenuItem.Text = "Clear ";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.ClearToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem1});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // aboutToolStripMenuItem1
+            // 
+            this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem1.Text = "About";
+            this.aboutToolStripMenuItem1.Click += new System.EventHandler(this.AboutToolStripMenuItem1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // rateTxtBox
+            // 
+            this.rateTxtBox.Location = new System.Drawing.Point(95, 133);
+            this.rateTxtBox.Name = "rateTxtBox";
+            this.rateTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.rateTxtBox.TabIndex = 2;
+            // 
+            // hourTxtBox
+            // 
+            this.hourTxtBox.Location = new System.Drawing.Point(95, 107);
+            this.hourTxtBox.Name = "hourTxtBox";
+            this.hourTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.hourTxtBox.TabIndex = 2;
+            // 
+            // hourLbl
+            // 
+            this.hourLbl.AutoSize = true;
+            this.hourLbl.Location = new System.Drawing.Point(51, 114);
+            this.hourLbl.Name = "hourLbl";
+            this.hourLbl.Size = new System.Drawing.Size(38, 13);
+            this.hourLbl.TabIndex = 3;
+            this.hourLbl.Text = "Hours:";
+            // 
+            // rateLbl
+            // 
+            this.rateLbl.AutoSize = true;
+            this.rateLbl.Location = new System.Drawing.Point(56, 140);
+            this.rateLbl.Name = "rateLbl";
+            this.rateLbl.Size = new System.Drawing.Size(33, 13);
+            this.rateLbl.TabIndex = 3;
+            this.rateLbl.Text = "Rate:";
+            // 
+            // computePayBtn
+            // 
+            this.computePayBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.computePayBtn.Location = new System.Drawing.Point(97, 159);
+            this.computePayBtn.Name = "computePayBtn";
+            this.computePayBtn.Size = new System.Drawing.Size(100, 23);
+            this.computePayBtn.TabIndex = 4;
+            this.computePayBtn.Text = "Compute Pay";
+            this.computePayBtn.UseVisualStyleBackColor = true;
+            this.computePayBtn.Click += new System.EventHandler(this.ComputePayBtn_Click);
+            // 
+            // empIDTxtBox
+            // 
+            this.empIDTxtBox.Location = new System.Drawing.Point(95, 81);
+            this.empIDTxtBox.Name = "empIDTxtBox";
+            this.empIDTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.empIDTxtBox.TabIndex = 5;
+            // 
+            // nameTxtBox
+            // 
+            this.nameTxtBox.Location = new System.Drawing.Point(95, 55);
+            this.nameTxtBox.Name = "nameTxtBox";
+            this.nameTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTxtBox.TabIndex = 5;
+            // 
+            // nameLbl
+            // 
+            this.nameLbl.AutoSize = true;
+            this.nameLbl.Location = new System.Drawing.Point(51, 62);
+            this.nameLbl.Name = "nameLbl";
+            this.nameLbl.Size = new System.Drawing.Size(38, 13);
+            this.nameLbl.TabIndex = 6;
+            this.nameLbl.Text = "Name:";
+            // 
+            // empLbl
+            // 
+            this.empLbl.AutoSize = true;
+            this.empLbl.Location = new System.Drawing.Point(19, 88);
+            this.empLbl.Name = "empLbl";
+            this.empLbl.Size = new System.Drawing.Size(70, 13);
+            this.empLbl.TabIndex = 6;
+            this.empLbl.Text = "Employee ID:";
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveBtn.Location = new System.Drawing.Point(97, 188);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(100, 23);
+            this.saveBtn.TabIndex = 7;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 319);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.empLbl);
+            this.Controls.Add(this.nameLbl);
+            this.Controls.Add(this.nameTxtBox);
+            this.Controls.Add(this.empIDTxtBox);
+            this.Controls.Add(this.computePayBtn);
+            this.Controls.Add(this.rateLbl);
+            this.Controls.Add(this.hourLbl);
+            this.Controls.Add(this.hourTxtBox);
+            this.Controls.Add(this.rateTxtBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Pay Calculator ";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.TextBox rateTxtBox;
+        private System.Windows.Forms.TextBox hourTxtBox;
+        private System.Windows.Forms.Label hourLbl;
+        private System.Windows.Forms.Label rateLbl;
+        private System.Windows.Forms.Button computePayBtn;
+        private System.Windows.Forms.TextBox empIDTxtBox;
+        private System.Windows.Forms.TextBox nameTxtBox;
+        private System.Windows.Forms.Label nameLbl;
+        private System.Windows.Forms.Label empLbl;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+        private System.Windows.Forms.Button saveBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace PayCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static decimal computePay(decimal hour, decimal rate)
+        {
+            if (hour > 40)
+            {
+                decimal OThour = hour - 40;
+                decimal OTsum = OThour * ((decimal)1.5 * rate);
+                hour = hour - OThour;
+                decimal sum = hour * rate;
+                sum = OTsum + sum;
+                return sum;
+            }
+            else
+            {
+                decimal sum = hour * rate;
+                return sum;
+            }
+        } // end of computePay
+
+        private void ComputePayBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //string name = nameTxtBox.Text;
+                //int employeeID = Convert.ToInt32(empIDTxtBox.Text);
+                decimal hour = Convert.ToDecimal(hourTxtBox.Text);
+                decimal rate = Convert.ToDecimal(rateTxtBox.Text);
+                decimal sum = computePay(hour, rate);
+                MessageBox.Show(Convert.ToString(sum));
+            }
+            catch (Exception wrongInput)
+            {
+                MessageBox.Show("Please check input and try again.");
+            }
+        }
+
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            //TextWriter txt = new StreamWriter("C:\\demo\\demo.txt");
+            //C:\Users\new user\Desktop\c#projects\PayCalculator
+            TextWriter txt = new StreamWriter("C:\\Users\\new user\\Desktop\\c#projects\\PayCalculator\\test.txt", true);
+            //TextWriter txt = new StreamWriter("C:\\PayCalculator\\test.txt");
+            txt.Write(nameTxtBox.Text + "\t");
+            txt.Write(empIDTxtBox.Text + "\t");
+            txt.Write(hourTxtBox.Text + "\t");
+            txt.Write(rateTxtBox.Text + "\t");
+            decimal hour = Convert.ToDecimal(hourTxtBox.Text); //Clean this up - shorten, make more elegant 
+            decimal rate = Convert.ToDecimal(rateTxtBox.Text); //
+            decimal sum = computePay(hour, rate);
+            txt.Write(sum);
+            txt.Write("\n");
+            txt.Close();
+            nameTxtBox.Clear();
+            empIDTxtBox.Clear();
+            hourTxtBox.Clear();
+            rateTxtBox.Clear();
+
+
+        }
+
+
+        //Menu Options
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ClearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            nameTxtBox.Clear();
+            empIDTxtBox.Clear();
+            hourTxtBox.Clear();
+            rateTxtBox.Clear();
+        }
+
+        private void AboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Calculates paycheck and stores to database.\nAuthor: Nate Nolting");
+                
+        }
+
+        
+    }
+}
